ec1e5036c030b4d3f88f67b5c5c491c3e07d0f4d
Christophe Gerard <chg502@usask.ca>
2023-01-03 07:47:29 -0600

    create navbar style, stack, link and logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Box } from '@mui/material'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-import {Feed, Navbar, VideoDetail, SearchFeed, ChannelDetail} from './components' 
+import {
+  Feed,
+  Navbar,
+  VideoDetail,
+  SearchFeed,
+  ChannelDetail,
+} from "./components";
 
 const App = () => (
-    <BrowserRouter>
-     <Box sx={{backgroundColor: '#000'}}>
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path ="/" exact element = {<Feed />} />
-        <Route path ="/video/:id" element = {<VideoDetail />} />
-        <Route path ="/channel/:id" element = {<ChannelDetail />} />
-        <Route path ="/search/:searchTerm" element = {<SearchFeed />} />
-
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
-     </Box>
-    </BrowserRouter>
-  );
+    </Box>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
