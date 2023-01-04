ac5ae32e2a4bc0dce04dc121115866f004abcf21
Christophe Gerard <chg502@usask.ca>
2023-01-04 06:43:31 -0600

    made sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
+const selectedCategory = "New";
+
 const sidebar = () => (
   <Stack
     direction="row"
@@ -10,10 +12,31 @@ const sidebar = () => (
       flexDirection: { md: "column" },
     }}
   >
-    {categories.map((categories) => (
-      <button>
-        <span>{categories.icon}</span>
-        <span>{categories.name}</span>
+    {categories.map((category) => (
+      // if button.name is selected button then set colour
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{
+            opacity: category.name === selectedCategory ? "1" : "0.5",
+          }}
+        >
+          {category.name}
+        </span>
       </button>
     ))}
   </Stack>
