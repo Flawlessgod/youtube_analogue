31e2a11faf6807771dfd95dd59137f8574c13266
Christophe Gerard <chg502@usask.ca>
2023-01-06 07:33:12 -0600

    display video titles

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,7 +17,13 @@ const VideoCard = ({
 }) => {
   //   console.log(videoId, snippet);
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { md: "320px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: "0",
+      }}
+    >
       {/* if theres a video get videoId else it must be a url */}
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         {/* ? sets errors on null paths to undefined */}
@@ -27,6 +33,27 @@ const VideoCard = ({
           sx={{ width: 350, height: 180 }}
         />
       </Link>
+
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "180px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.title.slice(0, 60)}
+          </Typography>
+        </Link>
+
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoVideoTitle.channelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
