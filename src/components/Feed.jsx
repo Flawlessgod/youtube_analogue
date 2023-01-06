60993c69e0cf8db55a5e82c0f9e6311bc74b2d15
Christophe Gerard <chg502@usask.ca>
2023-01-05 18:50:23 -0600

    created await function

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,9 +6,15 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    //q= "query equals" selectedCategory, .then is a promise for await
+    const data = fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then(
+      (data) => {
+        setVideos(data.items);
+      }
+    );
   }, [selectedCategory]);
 
   return (
@@ -45,7 +51,7 @@ const Feed = () => {
           <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
 
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
