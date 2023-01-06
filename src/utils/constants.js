93e55285d53d574f0dfb6ef7f75ee328823fd3cf
Christophe Gerard <chg502@usask.ca>
2023-01-06 09:09:32 -0600

    minor feed alter

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -17,6 +17,7 @@ export const logo = Frog;
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
+  { name: "Bobby Fingers", icon: <TheaterComedyIcon /> },
   { name: "JS Mastery", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
@@ -35,11 +36,9 @@ export const categories = [
   { name: "Crypto", icon: <DeveloperModeIcon /> },
 ];
 
-export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
-export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
-export const demoVideoUrl = "/video/GDa8kZLNhJ4";
-export const demoChannelTitle = "JavaScript Mastery";
-export const demoVideoTitle =
-  "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
-export const demoProfilePicture =
-  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
+export const demoThumbnailUrl = Frog;
+export const demoChannelUrl = "/@bobbyfingers";
+export const demoVideoUrl = "/watch?v=3aCMTpJx2cs";
+export const demoChannelTitle = "Bobby Fingers";
+export const demoVideoTitle = "Steven Seagal Choke Hold Diorama";
+export const demoProfilePicture = Frog;
