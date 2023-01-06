639614cdc262a5789a3f24d59f6fc0456b27f94f
Christophe Gerard <chg502@usask.ca>
2023-01-06 07:20:03 -0600

    display individual videoId's

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoVideoTitle,
+  demoVideoUrl,
+  demoChannelTitle,
+  demoChannelUrl,
+} from "../utils/constants";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  //   console.log(videoId, snippet);
+  return (
+    <Card>
+      {/* if theres a video get videoId else it must be a url */}
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        {/* ? sets errors on null paths to undefined */}
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 350, height: 180 }}
+        />
+      </Link>
+    </Card>
+  );
 };
 
 export default VideoCard;
